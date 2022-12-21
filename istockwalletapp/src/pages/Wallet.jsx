070dcf815144c7b875a311809537fc33f2dcfe1d
Matheus Zufi <matheuszufi@gmail.com>
2022-12-21 10:38:28 -0300

    Update Wallet

--- istockwalletapp/src/pages/Wallet.jsx
+++ istockwalletapp/src/pages/Wallet.jsx
@@ -13,9 +13,16 @@ function Wallet() {
         <header className="walletHeader">
             <h1>Carteiras</h1>
             <div className='stockInputDiv'>
-            <input type="text" placeholder='TICKER (EX: VALE3)' className='stockInput'></input>
+            <select type="text" placeholder='TICKER (EX: VALE3)' className='stockInput'>
+                <option value="Vale S.A.(VALE3)">Vale S.A.(VALE3)</option>
+                <option value="Itaú Unibanco(ITUB4)">Itaú Unibanco(ITUB4)</option>
+                <option value="Apple Inc(AAPL)">Apple Inc(AAPL)</option>
+                <option value="Microsoft Corp(MSFT)">Microsoft Corp(MSFT)</option>
+                <option value="Vanguard 500 Index Fund ETF(VOO)">Vanguard 500 Index Fund ETF(VOO)</option>
+                <option value="International Business Machine(IBM)">International Business Machine(IBM)</option>
+            </select>
             <FaSearch  className='stockIcon'/>
-       
+            <button className="btn btn-primary">Obter Dados</button>
             </div>
            
         </header>
