aaaed7d29e08539b7b10f14fbecce42b70257d7b
Matheus Zufi <matheuszufi@gmail.com>
2023-01-10 12:31:18 -0300

    up

--- istockwalletapp/src/pages/Wallet.jsx
+++ istockwalletapp/src/pages/Wallet.jsx
@@ -1,7 +1,7 @@
 
 import {Link, useNavigate} from 'react-router-dom'
-import { FaGoogleWallet, FaSearch } from 'react-icons/fa'
-
+import { FaGoogleWallet, FaSearch, FaArrowUp } from 'react-icons/fa'
+import Grafico from '../assets/grafico.png'
 
 function Wallet() {
  
@@ -13,18 +13,73 @@ function Wallet() {
         <header className="walletHeader">
             <h1>Carteiras</h1>
             <div className='stockInputDiv'>
-            <select type="text" placeholder='TICKER (EX: VALE3)' className='stockInput'>
-                <option value="Vale S.A.(VALE3)">Vale S.A.(VALE3)</option>
-                <option value="Itaú Unibanco(ITUB4)">Itaú Unibanco(ITUB4)</option>
-                <option value="Apple Inc(AAPL)">Apple Inc(AAPL)</option>
-                <option value="Microsoft Corp(MSFT)">Microsoft Corp(MSFT)</option>
-                <option value="Vanguard 500 Index Fund ETF(VOO)">Vanguard 500 Index Fund ETF(VOO)</option>
-                <option value="International Business Machine(IBM)">International Business Machine(IBM)</option>
-            </select>
-            <FaSearch  className='stockIcon'/>
-            <button className="btn btn-primary">Obter Dados</button>
+                <select type="text" placeholder='TICKER (EX: VALE3)' className='stockInput'>
+                    <option value="Vale S.A.(VALE3)">Vale S.A.(VALE3)</option>
+                    <option value="Itaú Unibanco(ITUB4)">Itaú Unibanco(ITUB4)</option>
+                    <option value="Apple Inc(AAPL)">Apple Inc(AAPL)</option>
+                    <option value="Microsoft Corp(MSFT)">Microsoft Corp(MSFT)</option>
+                    <option value="Vanguard 500 Index Fund ETF(VOO)">Vanguard 500 Index Fund ETF(VOO)</option>
+                    <option value="International Business Machine(IBM)">International Business Machine(IBM)</option>
+                </select>
+                <FaSearch  className='stockIcon'/>
+                <button className="btn btn-primary">Obter Dados</button>
             </div>
            
+            <div id='stockCardDiv'>
+                <div id="stockCard">
+                    <div id="cardHeader">
+                        <div id=''>
+                        <h1>ABEV3</h1>
+                        </div>
+                        <div id="cardHeaderVolatility">
+                            <div id='cardHeaderVolatilityBtn'>
+                                <button>D</button>
+                                <button>S</button>
+                                <button>M</button>
+                                <button>1A</button>
+                                <button>2A</button>
+                                <button>5A</button>     
+                            </div>
+                            <div id="cardHeaderVolatilityInput">
+                            <h3><FaArrowUp /> 2,87%</h3>
+                            </div>
+
+                        </div>
+                    </div>
+               
+                    <div id='graficoDiv'>
+                    <img id='grafico' src={Grafico} />
+                    </div>
+                    <div id="compraVendaDiv">
+                    <div id="compra">Compra: R$20,45</div>
+                    <div id='venda'>Venda: R$41,52</div>
+                    </div>
+                
+
+                    <div id='cardHoverDiv'>
+                        <p>FC Desc.:</p>
+                    </div>
+                </div>
+    
+                <div id="stockCard"></div>
+                <div id="stockCard"></div>
+                <div id="stockCard"></div>
+                <div id="stockCard"></div>
+                <div id="stockCard"></div>
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+
         </header>
 
         </>
