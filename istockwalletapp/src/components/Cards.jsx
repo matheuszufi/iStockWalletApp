17a2acbfc62a7f2d33da3ad148cee0ae1a81d8b2
Matheus Zufi Leite <matheuszufi@gmail.com>
2023-02-25 15:33:37 -0300

    update

---
+++ istockwalletapp/src/components/Cards.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../index.css';
+
+
+const Cards = () => {
+    const [results, setResults] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+  
+        try {
+          const response = await axios.get("https://brapi.dev/api/quote/AESB3,PETR4,MGLU3,TAEE11")
+          const results = response.data.results
+          
+          setResults(results);
+  
+          setIsLoading(false);
+        } catch (error) {
+          console.error('Erro ao fazer requisição:', error);
+          setIsLoading(false);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+    return (
+        isLoading ? (
+          <p>Carregando...</p>
+        ) : (
+        results.map(result => {
+          console.log(result)
+          return <div key={result.symbol} className='cartao'>
+            <div className='cartao-header'>
+              <div className='cartao-header-left'>
+                  <h1>{result.longName}</h1>
+                  <p>{result.symbol}</p>
+              </div>
+              <div className='cartao-header-right'>
+                  <p>Valor atual:</p>
+                  <h2>R${result.regularMarketPrice.toFixed(2)}</h2>
+              </div>
+            </div>
+          </div>
+        }))
+    )
+  };
+  
+  export default Cards;
+  
--- istockwalletapp/src/pages/Wallet.jsx
+++ istockwalletapp/src/pages/Wallet.jsx
@@ -1,51 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import '../index.css';
+import React from 'react'
+import Cards from '../components/Cards'
 
-
-
-const Abev3Itub4ValueScraper = () => {
-  const [results, setResults] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-
-      try {
-        const response = await axios.get("https://brapi.dev/api/quote/PETR4,MGLU3,TAEE11")
-        const results = response.data.results
-        
-        setResults(results);
-
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Erro ao fazer requisição:', error);
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+function Wallet() {
   return (
-      isLoading ? (
-        <p>Carregando...</p>
-      ) : (
-      results.map(result => {
-        return <div key={result.symbol} className='cartao'>
-          <div className='cartao-header'>
-            <div className='cartao-header-left'>
-                <h1>{result.longName}</h1>
-                <p>{result.symbol}</p>
-            </div>
-            <div className='cartao-header-right'>
-                <p>Valor atual:</p>
-                <h2>R${result.regularMarketPrice.toFixed(2)}</h2>
-            </div>
-          </div>
-        </div>
-      }))
+    <div><Cards/></div>
   )
-};
+}
 
-export default Abev3Itub4ValueScraper;
+export default Wallet
